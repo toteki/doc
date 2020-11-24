5f49bda54da940b55de0bcc982acc84f821b725f
Adam Moser <63419657+toteki@users.noreply.github.com>
2020-11-23 21:50:06 -0700

    working files; before streamlining

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # doc
-Automated documentation test
+
+Created to be able to generate godoc html pages, except as a flat file without having to serve.
+
+Requirements:
+	- godoc installed (not go doc, but godoc) and usable
+	- go itself (to build the doc.go script)
+
+How do:
+	- run script
+
+What do:
+	- Prompts user to enter a package to document, like github.com/toteki/wiz
+	- Uses 'go get -u' on that package to bring it up to date
+	- Builds to go.doc tool here
+	- Runs the go.doc tool, which fixes the godoc html output (style and broker links)
+	- Displays the resulting docfinal.html
+
+Annoyances:
+	- Since this uses go get to fetch the package, you can't use the tool on local changes, or branch changes. The clunky but effective solution to this might just be to push changes, THEN run this and push documentation.
---
+++ doc.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"github.com/toteki/wiz"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func main() {
+
+	args := wiz.Args()
+
+	wiz.Green("Reading file " + args[0])
+	b, err := wiz.ReadFile(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s0 := []byte("<script>\n")
+	s1 := []byte("\n</script>")
+
+	wiz.Green("Reading file godocs.js")
+	godocs, err := wiz.ReadFile("godocs.js")
+	if err != nil {
+		log.Fatal(err)
+	}
+	godocs = append(s0, godocs...)
+	godocs = append(godocs, s1...)
+
+	wiz.Green("Reading file jquery.js")
+	jquery, err := wiz.ReadFile("jquery.js")
+	if err != nil {
+		log.Fatal(err)
+	}
+	jquery = append(s0, jquery...)
+	jquery = append(jquery, s1...)
+
+	c0 := []byte("<style>\n")
+	c1 := []byte("\n</style>")
+
+	wiz.Green("Reading file style.css")
+	style, err := wiz.ReadFile("style.css")
+	if err != nil {
+		log.Fatal(err)
+	}
+	style = append(c0, style...)
+	style = append(style, c1...)
+
+	//
+	//
+	//
+
+	line := regexp.MustCompile(".*")
+
+	lines := line.FindAll(b, -1)
+
+	lines = purge(lines, "<div id=\"footer\">", "</div>")
+
+	//
+	//
+	//
+
+	lines = replace(lines, "using GOPATH mode", []byte{})
+	lines = replace(lines, "<script src=\"/lib/godoc/jquery.js\" defer></script>", jquery)
+	lines = replace(lines, "<script src=\"/lib/godoc/godocs.js\" defer></script>", godocs)
+	lines = replace(lines, "<link type=\"text/css\" rel=\"stylesheet\" href=\"/lib/godoc/style.css\">", style)
+
+	//
+	//
+	//
+
+	out := []byte{}
+	for _, l := range lines {
+		out = append(out, l...)
+		out = append(out, []byte("\n")...)
+	}
+	wiz.Green("Finished turning into single file")
+
+	//Next, should transform
+	//<a href="/src/github.com/toteki/wiz/ASCII.go?s=653:688#L14">ASCII</a>
+	//to
+	//ASCII
+	//following the rule that, if the href starts with /, eliminate
+
+	//The regex that will match them
+	refspotter := regexp.MustCompile("<a href=\"/[^\"]*\">")
+	out = refspotter.ReplaceAllLiteral(out, []byte("<a doomed href>"))
+	wiz.Red("Spotting all broken hyperlinks")
+
+	//The regex that will kill them
+	refkiller := regexp.MustCompile("<a doomed href>[^<]*</a>")
+	out = refkiller.ReplaceAllFunc(out, striplink)
+	wiz.Red("Killing all broker hyperlinks")
+
+	err = wiz.WriteFile(args[1], out)
+	if err != nil {
+		log.Fatal(err)
+	}
+	wiz.Green("Wrote file " + args[1])
+}
+
+func striplink(in []byte) []byte {
+	s := string(in)
+	s = strings.TrimPrefix(s, "<a doomed href>")
+	s = strings.TrimSuffix(s, "</a>")
+	return []byte(s)
+}
+
+func replace(lines [][]byte, line string, file []byte) [][]byte {
+	//Replaces a given line in a [][]byte line-separated file, with not
+	//  one line but an entire byte stream. In this case a file.
+	out := [][]byte{}
+	for _, l := range lines {
+		if string(l) == line {
+			wiz.Purple("Replacing line: ", line)
+			out = append(out, file) //add the file
+		} else {
+			out = append(out, l) //add the existing line
+		}
+	}
+	return out
+}
+
+func purge(lines [][]byte, line1, line2 string) [][]byte {
+	//Purges all lines in a [][]byte line-separated file, between two specified
+	//  lines of text. (Purge eliminates the specified lines themselves too)
+	active := false
+	out := [][]byte{}
+	for _, l := range lines {
+		if !active {
+			//If not currently purging between two lines
+			if string(l) == line1 {
+				active = true //Start purging
+			}
+		}
+		if active {
+			//If currently purging between two lines
+			if string(l) == line2 {
+				active = false //Stop purging
+			}
+		}
+		//If not purging AFTER checks
+		if !active {
+			out = append(out, l)
+		}
+	}
+	return out
+}
